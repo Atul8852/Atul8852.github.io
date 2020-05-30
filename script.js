d998ab70897985440e77ed315f16d9f0fe222770
Atul8852 <62022049+Atul8852@users.noreply.github.com>
2020-05-30 10:46:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var a=prompt("Enter UserName");
  var b=prompt("Enter password");
-  	if(a== "corona" && b=="corona")
+  	if(a== "Immatured bachha" && b=="Immatured bachha")
   	{
   	    window.location.href="start.html";
   	}
